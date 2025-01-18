361a4528cad7fcd3d86664bed45c4ddd75bbf30c
Joey Parshley <joeyparshley@gmail.com>
2025-01-17 20:22:34 -0500

    Initial scaffolding and operator functions

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+/**
+ * start by creating functions for the following items and testing them in your browser’s console:
+ *  -   add
+ *  -   subtract
+ *  -   multiply
+ *  -   divide
+ */
+
+/**
+ * add
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns sum of two numbers
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ * subtract
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns difference of two numbers
+ */
+function subtract(a, b) {
+  return a - b;
+}
+
+/**
+ * multiply
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns product of two numbers
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
+/**
+ * divide
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns quotient of two numbers
+ */
+function divide(a, b) {
+  return a / b;
+}
